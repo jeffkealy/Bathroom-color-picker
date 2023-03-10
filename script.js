5cdb6349d723aca8bf44f787f5a0a4c50dd9572a
jeffkealy <jeffkealy@yahoo.com>
2023-03-10 17:45:55 -0600

    both walls modifiable

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
 $(document).ready(function () {
-  Coloris({
+  Coloris.setInstance(".both-input", {
     themeMode: "dark",
     focusInput: false,
-    onChange: (color) => $(".front").css("background-color", color),
+    onChange: (color) => {
+      console.log("both");
+      $(".front.vanity").css("background-color", color);
+      $(".front.bath").css("background-color", color);
+    },
   });
-  $("#brightnessSlider input").change(function () {
-    b = $(this).val();
-    $(".wall").css("filter", `brightness(${b}%)`);
-    $("#brightnessSlider span").text(b);
+  Coloris.setInstance(".bath-input", {
+    themeMode: "dark",
+    focusInput: false,
+    onChange: (color) => {
+      console.log("bath");
+      $(".front.bath").css("background-color", color);
+    },
+  });
+  Coloris.setInstance(".vanity-input", {
+    themeMode: "dark",
+    focusInput: false,
+    onChange: (color) => {
+      console.log("bath");
+      $(".front.vanity").css("background-color", color);
+    },
+  });
+
+  //set both brightness
+  $(".both .brightness-slider input").change(function () {
+    let b = $(this).val();
+    $(".wall.vanity, .wall.bath").css("filter", `brightness(${b}%)`);
+    $(".vanity .brightness-slider input, .bath .brightness-slider input").val(b);
+    $(".vanity .brightness-slider span, .bath .brightness-slider span").text(b);
+  });
+  //set bath wall brightness
+  $(".bath .brightness-slider input").change(function () {
+    let b = $(this).val();
+    $(".wall.bath").css("filter", `brightness(${b}%)`);
+    $(".bath .brightness-slider span").text(b);
+  });
+  //set vanity wall brightness
+  $(".vanity .brightness-slider input").change(function () {
+    let b = $(this).val();
+    $(".wall.vanity").css("filter", `brightness(${b}%)`);
+    $(".vanity .brightness-slider span").text(b);
   });
 });
